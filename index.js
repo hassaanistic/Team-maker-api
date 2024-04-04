64e71aa4b346d28a91b90422578f5776de64a303
unknown <imhassaan.dev@gmaile.com>
2024-04-05 04:53:46 +0500

    apis created

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(cors(
 ));
 
 // Connect to MongoDB
-// mongoose.connect(process.env.DATABASE_URL).then(() => console.log('Connected to MongoDB'))
-//     .catch(error => console.error('Error connecting to MongoDB:', error));
+mongoose.connect(process.env.DATABASE_URL).then(() => console.log('Connected to MongoDB'))
+    .catch(error => console.error('Error connecting to MongoDB:', error));
 
     
 app.get('/', async (req, res) => {
